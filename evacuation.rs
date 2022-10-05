77fd78e5ee1e37728634a6a3fbcafea141a5542c
hlcalli <hannahcallihan@gmail.com>
2022-10-05 10:44:20 -0400

    file creation

--- evacuation.rs
+++ evacuation.rs
@@ -3,3 +3,11 @@
 // Course: CSE 4250, Fall 2022
 // Project: Proj2, Manatee Evacuation
 // Implementation: rust
+
+
+
+
+// take input with arrays (size/age)
+// call sort 
+// create binary search tree
+// find best match - same age, smaller male-->larger female (closest distance in size)
