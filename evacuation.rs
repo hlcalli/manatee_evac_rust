af94d4bde70fce15c27067e66f3fac5b18309894
MichaelHeinzman <seth@heinzman.net>
2022-10-11 15:05:00 -0400

    Completed grabbing inputs.

--- evacuation.rs
+++ evacuation.rs
@@ -3,46 +3,39 @@
 // Course: CSE 4250, Fall 2022
 // Project: Proj2, Manatee Evacuation
 // Implementation: rust
+use std::io;
+fn fetch_number_in_each_row() -> usize {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Need number of names");
 
+    return input.trim().parse().expect("Need a number");
+}
+fn fetch_row_array(text: &str, row_length: usize) -> Vec<String> {
+    let mut input = String::new();
+    let mut row = vec![String::new(); row_length];
+
+    println!("{}", text);
+    io::stdin().read_line(&mut input).expect("Need name");
+
+    for i in 0..row_length {
+        row[i] = input.as_str().chars().nth(i).unwrap().to_string()
+    }
+
+    return row;
+}
 
 fn main() {
-    // take input with arrays (size/age)
-    use io::BufRead;
-    use std::io; // necessary to have `.lines()` on
-                 // locked stdin available
-        let stdin = io::stdin();
-        let mut lines = stdin.lock().lines();
-        let numberofnames: usize = lines
-            .next()
-            .expect("Need input from stdin") // checking for any line being present
-            .expect("Need number of names")
-            .trim()
-            .parse()
-            .expect("Need a number");
-        let names = lines
-            .take(numberofnames)
-            .map(|line| line.expect("Need name").trim().into())
-            .collect::<Vec<String>>();
-        if names.len() < numberofnames {
-            panic!("Need more names, only got {}!", names.len());
-        }
-        
-    // create manatee struct
-    struct Manatee {
-        size: f32,
-        age: f32,
-        id: f32,
-    }
-    
+    // Take input with arrays (size/age)
+
+    let _number_in_each_row: usize = fetch_number_in_each_row();
+    let _female_age_row = fetch_row_array("Please Enter Female Age Row :", _number_in_each_row);
+    println!("Number in Each Row: {}", _number_in_each_row);
+    println!("Female Age Row : {:?}", _female_age_row);
     // create binary search tree
-    
-    
+
     // find best match - same age, smaller male-->larger female (closest distance in size)
-    
-    
+
     // print output (make sure no '[]')
-    
-    }
-
-
-
+}
