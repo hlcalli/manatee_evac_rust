7d5ba669bd9ec3544ccff15a69eadeba9d6bc2d2
MichaelHeinzman <seth@heinzman.net>
2022-10-11 15:10:54 -0400

    Updated comments again lol.

--- evacuation.rs
+++ evacuation.rs
@@ -3,17 +3,23 @@
 // Course: CSE 4250, Fall 2022
 // Project: Proj2, Manatee Evacuation
 // Implementation: rust
+
 use std::io;
+
 fn fetch_number_in_each_row() -> usize {
+    // Initializes type of input.
     let mut input = String::new();
+
+    // Gets whatever the user types and shoves it in input.
     io::stdin()
         .read_line(&mut input)
         .expect("Need number of names");
 
+    // Sets the input to a number. In this case a usize (That's what the site did).
     return input.trim().parse().expect("Need a number");
 }
 fn fetch_row_array(text: &str, row_length: usize) -> Vec<String> {
-    // Where the input will be stored
+    // Initializes type of input.
     let mut input = String::new();
 
     // Establishes a vector of type String::new() and with a size of row_length.
@@ -37,7 +43,7 @@ fn main() {
     // Gets the number in each row.
     let _number_in_each_row: usize = fetch_number_in_each_row();
 
-    // Fetches each user input for the rows. (Female Age is the only one setup right now.)
+    // Fetches each user input for the rows (Female Age is the only one setup right now).
     let _female_age_row = fetch_row_array("Please Enter Female Age Row :", _number_in_each_row);
     println!("Number in Each Row: {}", _number_in_each_row);
     println!("Female Age Row : {:?}", _female_age_row);
