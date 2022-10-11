17006538a7817cbaf394e3b9b9ee7f99d0cd4f74
hlcalli <hannahcallihan@gmail.com>
2022-10-11 15:14:44 -0400

    seth update

--- evacuation.rs
+++ evacuation.rs
@@ -4,45 +4,49 @@
 // Project: Proj2, Manatee Evacuation
 // Implementation: rust
 
+use std::io;
+
+fn fetch_number_in_each_row() -> usize {
+    // Initializes type of input.
+    let mut input = String::new();
+
+    // Gets whatever the user types and shoves it in input.
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Need number of names");
+
+    // Sets the input to a number. In this case a usize (That's what the site did).
+    return input.trim().parse().expect("Need a number");
+}
+fn fetch_row_array(text: &str, row_length: usize) -> Vec<String> {
+    // Initializes type of input.
+    let mut input = String::new();
+
+    // Establishes a vector of type String::new() and with a size of row_length.
+    let mut row = vec![String::new(); row_length];
+
+    // Prints out what you want to ask the user.
+    println!("{}", text);
+
+    // Gets whatever the user types and shoves it in input.
+    io::stdin().read_line(&mut input).expect("Need name");
+
+    // Iterates row_length times and sets the character to row[i]
+    for i in 0..row_length {
+        row[i] = input.as_str().chars().nth(i).unwrap().to_string()
+    }
+
+    return row;
+}
 
 fn main() {
-    // take input with arrays (size/age)
-    use io::BufRead;
-    use std::io; // necessary to have `.lines()` on
-                 // locked stdin available
-        let stdin = io::stdin();
-        let mut lines = stdin.lock().lines();
-        let numberofnames: usize = lines
-            .next()
-            .expect("Need input from stdin") // checking for any line being present
-            .expect("Need number of names")
-            .trim()
-            .parse()
-            .expect("Need a number");
-        let names = lines
-            .take(numberofnames)
-            .map(|line| line.expect("Need name").trim().into())
-            .collect::<Vec<String>>();
-        if names.len() < numberofnames {
-            panic!("Need more names, only got {}!", names.len());
-        }
-        
-    // create manatee struct
-    struct Manatee {
-        size: f32,
-        age: f32,
-        id: f32,
-    }
-    
-    // create binary search tree
-    
-    
-    // find best match - same age, smaller male-->larger female (closest distance in size)
-    
-    
-    // print output (make sure no '[]')
-    
-    }
+    // Gets the number in each row.
+    let _number_in_each_row: usize = fetch_number_in_each_row();
 
+    // Fetches each user input for the rows (Female Age is the only one setup right now).
+    let _female_age_row = fetch_row_array("Please Enter Female Age Row :", _number_in_each_row);
 
-
+    // Prints out variables above for checking (You have to use {:?} for vectors).
+    println!("Number in Each Row: {}", _number_in_each_row);
+    println!("Female Age Row : {:?}", _female_age_row);
+}
